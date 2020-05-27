ad4acd6bffdfd951bcbdc348b27843f1e64bf7b8
Richard Kawala <rkawala@gmail.com>
2020-05-26 19:37:34 -0700

    Add some tests for an earlier part of chapter 2.

--- ch2-tests.lisp
+++ ch2-tests.lisp
@@ -1,5 +1,26 @@
 (in-package #:touretzky)
 
+(alexandria:define-constant honk-list '(honk if you like geese) :test #'equal)
+
+(deftest ch2-7
+  (testing "long input"
+    (flet ((my-second (x) (first (rest x))))
+      (ok (eq 'if (my-second honk-list))))))
+
+(deftest ch2-8
+  (testing "my-third using first and two rests"
+    (flet ((my-third (x) (first (rest (rest x)))))
+      (ok (eq 'you (my-third honk-list))))))
+
+(deftest ch2-9
+  (testing "my-third using second"
+    (flet ((my-third (x) (second (rest x))))
+      (ok (eq 'you (my-third honk-list))))))
+
+(deftest ch2-12
+  (testing "c...r for fourth position"
+    (ok (eq 'like (cadddr honk-list)))))
+
 (deftest ch2-13
     (testing "fun in the sun"
              (let ((a1 '(((fun)) (in the) (sun))))
