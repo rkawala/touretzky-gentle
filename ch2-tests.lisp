98638dde1e93d0f3c5b294fd36e0a9f6ac8b5929
Richard Kawala <rkawala@gmail.com>
2020-05-28 09:57:10 -0700

    Change numbering scheme for tests.
    Learning experience 1: I don't have to stick to a naming convention that
    follows the normal identifier rules. Lisp symbol names can have periods
    in them, no problem. Old habits die hard.
    
    Learning experience 2: Changing a deftest's symbol doesn't get rid of the
    old test in the image. You have to restart SBCL and reload.

--- ch2-tests.lisp
+++ ch2-tests.lisp
@@ -2,26 +2,26 @@
 
 (alexandria:define-constant honk-list '(honk if you like geese) :test #'equal)
 
-(deftest ch2-7
+(deftest ex2.7
   (testing "long input"
     (flet ((my-second (x) (first (rest x))))
       (ok (eq 'if (my-second honk-list))))))
 
-(deftest ch2-8
+(deftest ex2.8
   (testing "my-third using first and two rests"
     (flet ((my-third (x) (first (rest (rest x)))))
       (ok (eq 'you (my-third honk-list))))))
 
-(deftest ch2-9
+(deftest ex2.9
   (testing "my-third using second"
     (flet ((my-third (x) (second (rest x))))
       (ok (eq 'you (my-third honk-list))))))
 
-(deftest ch2-12
+(deftest ex2.12
   (testing "c...r for fourth position"
     (ok (eq 'like (cadddr honk-list)))))
 
-(deftest ch2-13
+(deftest ex2.13
     (testing "fun in the sun"
              (let ((a1 '(((fun)) (in the) (sun))))
                (ok (eq 'fun (caaar a1)))
@@ -29,7 +29,7 @@
                (ok (eq 'the (cadadr a1)))
                (ok (eq 'sun (caaddr a1))))))
 
-(deftest ch2-15
+(deftest ex2.15
   (testing "more c...r"
     (let ((a1 '((a b) (c d) (e f))))
       (ok (equal '(a b) (car a1)))
@@ -42,12 +42,12 @@
       (ok (equal '(f) (cdaddr a1)))
       (ok (eq 'f (car (cdaddr a1)))))))
 
-(deftest lisp-not-like-java
+(deftest sec2.10.4
   (testing "CAR and CDR of nil return nil rather than throwing an exception"
     (ok (null (car nil)))
     (ok (null (cdr nil)))))
 
-(deftest ch2-17
+(deftest ex2.17
   (testing "More CAR and CDRs"
     (ok (eq 'post (car '(post no bills))))
     (ok (equal '(no bills) (cdr '(post no bills))))
@@ -57,6 +57,7 @@
     (ok (equal '((no bills)) (cdr '(post (no bills)))))
     (ok (null (cdr '((post no bills)))))
     (ok (null (car nil)))))
+
 (deftest array-length
   (testing "array length"
     (let ((ary #(1 2 3 4 5)))
