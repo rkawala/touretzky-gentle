56b818d9eb730acf3ee5358e36b9bdb49a058117
Richard Kawala <rkawala@gmail.com>
2020-06-10 17:13:12 -0700

    Continue ex5.6: write throw-dice.

--- ch5-tests.lisp
+++ ch5-tests.lisp
@@ -2,7 +2,8 @@
 
 (defparameter *generator-lambda* (lambda () (random 6)))
 
-(labels ((throw-die () (funcall *generator-lambda*)))
+(labels ((throw-die () (funcall *generator-lambda*))
+         (throw-dice () (list (throw-die) (throw-die))))
   (deftest ex5.6
     (testing "throw-die"
       (let* ((rolls '(2 5)) (*generator-lambda* (lambda () (pop rolls))))
@@ -10,4 +11,7 @@
         (ok (= 2 (throw-die)))
         (ok (= 5 (throw-die)))
         (ok (zerop (length rolls)))
-        (ok (null (throw-die)))))))
+        (ok (null (throw-die)))))
+    (testing "throw-dice"
+      (let* ((rolls '(4 3)) (*generator-lambda* (lambda () (pop rolls))))
+        (ok (equal '(4 3) (throw-dice)))))))
